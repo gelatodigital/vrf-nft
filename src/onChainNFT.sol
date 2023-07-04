d18fe8926b840cfd29c64bf3d89f5cd1f472f374
Darius Foodeei <darius.foodei@gmail.com>
2023-07-04 17:09:36 +0200

    added chainlink vrf helper functions

--- src/onChainNFT.sol
+++ src/onChainNFT.sol
@@ -3,9 +3,11 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "openzeppelin/token/ERC721/extensions/ERC721Enumerable.sol";
 import "openzeppelin/access/Ownable.sol";
+import "chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "./Base64.sol";
 
-contract onChainNFT is ERC721Enumerable, Ownable {
+contract onChainNFT is ERC721Enumerable, Ownable, VRFConsumerBaseV2 {
     using Strings for uint256;
 
     bool public paused = false;
@@ -20,11 +22,75 @@ contract onChainNFT is ERC721Enumerable, Ownable {
         string value;
     }
 
-    //string[] public wordValues = ["accomplish", "accepted", "absolutely", "admire", "achievment", "active"];
-    // TODO: Replace random num with chainlink VRF
-    constructor() ERC721("onChainNFT", "OCN") {}
+    // VRF Config (GOERLI)
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
 
-    // public
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+
+    mapping(uint256 => RequestStatus) public s_requests;
+
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    uint64 s_subscriptionId;
+
+    // past requests Id.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    // The gas lane to use, which specifies the maximum gas price to bump to. (GOERLI)
+    bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+
+    uint32 callbackGasLimit = 100000;
+
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
+
+    // For this example, retrieve 2 random values in one request.
+    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+    uint32 numWords = 2;
+
+    //string[] public wordValues = ["accomplish", "accepted", "absolutely", "admire", "achievment", "active"];
+    constructor(uint64 subscriptionId)
+        VRFConsumerBaseV2(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D)
+        ERC721("onChainNFT", "OCN")
+    {
+        COORDINATOR = VRFCoordinatorV2Interface(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D);
+        s_subscriptionId = subscriptionId;
+    }
+
+    // Assumes the subscription is funded sufficiently
+    function requestRandomWords() external onlyOwner returns (uint256 requestId) {
+        requestId =
+            COORDINATOR.requestRandomWords(keyHash, s_subscriptionId, requestConfirmations, callbackGasLimit, numWords);
+        s_requests[requestId] = RequestStatus({randomWords: new uint256[](0), exists: true, fulfilled: false});
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
+    function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    function getRequestStatus(uint256 _requestId)
+        external
+        view
+        returns (bool fulfilled, uint256[] memory randomWords)
+    {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+
     function mint(string memory _userText) public payable {
         uint256 supply = totalSupply();
         bytes memory strBytes = bytes(_userText);
@@ -59,6 +125,7 @@ contract onChainNFT is ERC721Enumerable, Ownable {
         return result;
     }
 
+    // TODO: Replace random num with chainlink VRF
     function randomNum(uint256 _mod, uint256 _seed, uint256 _salt) public view returns (uint256) {
         uint256 num = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, _seed, _salt))) % _mod;
         return num;
